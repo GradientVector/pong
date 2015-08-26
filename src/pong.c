eec5bb9bc009be2ddb07dcb6cb34280212aa0f08
John Harvey <thegradientvector@gmail.com>
2015-08-26 00:21:05 -0500

    Added SDL2 to the project.

--- src/pong.c
+++ src/pong.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <SDL.h>
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
 
 int main(void) {
-	puts("Hello World"); /* prints Hello World */
+	SDL_Window *sdlWindow;
+	SDL_Renderer *sdlRenderer;
+	SDL_CreateWindowAndRenderer(0, 0, SDL_WINDOW_MAXIMIZED, &sdlWindow, &sdlRenderer);
+
+	SDL_SetRenderDrawColor(sdlRenderer, 0, 0, 0, 255);
+	SDL_RenderClear(sdlRenderer);
+	SDL_RenderPresent(sdlRenderer);
+
 	return EXIT_SUCCESS;
 }
